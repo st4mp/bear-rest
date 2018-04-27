fc742bacb643610a4ad2f471a0df06f40eb4d621
st4mp <5710110617@psu.ac.th>
2018-04-27 15:45:29 +0700

    server support deleting

--- server/src/index.js
+++ server/src/index.js
@@ -1,23 +1,31 @@
-var express = require('express');
-var app = express();
-var router = express.Router();
-var bodyParser = require('body-parser');
+var express = require('express'); 
+var app = express(); 
+var router = express.Router(); 
+var bodyParser = require('body-parser'); 
 var cors = require('cors');
+  
 var bears = [
-    {id: 1,name: "TU"},
-    {id: 2,name: "PRAYUT"}
-];
-router.route('/bears')
-    .get(function(req, res){
+    { id: '1', name: 'TU' },
+    { id: '2', name: 'PRAYUT' }
+]; 
+  
+router.route('/bears') 
+    .get(function(req, res) {
         res.send(bears);
     })
-    .post(function(req, res) {
-    var bear = {};
-    bear.name = req.body.name;
-    bears.push(bear);
-    res.json({ message: 'Bear created!' });
-});
+    .post(function(req, res) { 
+        var bear = {}; 
+        bear.name = req.body.name; 
+        bears.push(bear); 
+        res.json({ message: 'Bear created!' }); 
+    })
+router.route('/bears/:id')
+    .delete(function(req, res){
+        bears = bears.filter(b => b.id !== req.params.id)
+        res.json({ message: 'Bear deleted!' }); 
+    }) 
+
 app.use(cors());
-// all of our routes will be prefixed with /api
-app.use('/api', bodyParser.json(), router);
+// all of our routes will be prefixed with /api 
+app.use('/api', bodyParser.json(), router); 
 app.listen(8000);
